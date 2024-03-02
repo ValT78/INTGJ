3371c0f7dd2dbb96a8d1ec49d9e795e314ceb2a9
ValT78 <118465560+ValT78@users.noreply.github.com>
2024-03-02 21:24:14 +0100

    uiPorter

---
+++ Assets/Scripts/UIMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMove : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float killHeight;
+    [SerializeField] private RectTransform ui;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ui.position += new Vector3(0, moveSpeed * Time.deltaTime, 0);
+        print(ui.position.y);
+        if(ui.position.y > killHeight)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
