607a6cc7a8642608cfca2e68edd91504e9f02004
Sophie-2023 <sophie.siniavski@gmail.com>
2024-03-02 13:43:08 +0100

    Frigo

--- Assets/Scripts/OuvertureObjet.cs
+++ Assets/Scripts/OuvertureObjet.cs
@@ -7,10 +7,15 @@ public class OuvertureObjet : MonoBehaviour
     [SerializeField] private Vector3 rotationAxis = Vector3.up; // Axe de rotation
     [SerializeField] private float rotationSpeed = 50f; // Vitesse de rotation en degrés par seconde
     [SerializeField] private int targetAngle = 90;
+    
     private float totalAngleRotated = 0;
+    [SerializeField] public bool frigoDébloqué = false; 
+
+
+
     void Update()
     {
-        if (totalAngleRotated < targetAngle)
+        if (totalAngleRotated < targetAngle && frigoDébloqué)
         {
             transform.RotateAround(transform.position, rotationAxis, rotationSpeed * Time.deltaTime);
             totalAngleRotated += Time.deltaTime * rotationSpeed;
