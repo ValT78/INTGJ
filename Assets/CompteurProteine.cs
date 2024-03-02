c49eceb72b1eae99f95b7f506b55de73a071dce8
ValT78 <118465560+ValT78@users.noreply.github.com>
2024-03-02 14:31:45 +0100

    police + sprite

--- Assets/CompteurProteine.cs
+++ Assets/CompteurProteine.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CompteurProteine : MonoBehaviour
 {
     TextMeshProUGUI textmeshpro;
-    PorterObjet player;
+    private PorterObjet player;
     void Start()
     {
         player=FindObjectOfType<PorterObjet>();
