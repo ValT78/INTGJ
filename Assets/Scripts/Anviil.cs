359083bf36de1bf284cbdbf09881fc22fbf2e3e4
Sophie-2023 <sophie.siniavski@gmail.com>
2024-03-03 15:18:51 +0100

    Correction bug enclume

--- Assets/Scripts/Anviil.cs
+++ Assets/Scripts/Anviil.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Anviil : MonoBehaviour
 {
     [SerializeField] private GameObject key;
+    [SerializeField] private GameObject enclume;
     private SoundManager soundManager;
 
     private void Start()
@@ -19,6 +20,7 @@ public class Anviil : MonoBehaviour
             StartCoroutine(apiculteur.SmoothSquash());
             Instantiate(key, apiculteur.transform.position, Quaternion.identity);
             soundManager.PlaySound(soundManager.anvil);
+            Destroy(enclume);
         }
     }
 }
