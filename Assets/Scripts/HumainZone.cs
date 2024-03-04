5efc19b1e7ea8bb8d29ea5b7efaf0509cb3cf253
Helo1204 <heloise.havy@gmail.com>
2024-03-04 18:09:11 +0100

    same

--- Assets/Scripts/HumainZone.cs
+++ Assets/Scripts/HumainZone.cs
@@ -27,7 +27,7 @@ public class HumainZone : MonoBehaviour
     }
     private IEnumerator Explosion()
     {
-        yield return new WaitForSeconds(2.6f);
+        yield return new WaitForSeconds(2.8f);
         Instantiate(proteine, this.transform.position + offset, Quaternion.identity);
         Destroy(this.gameObject);
     }
